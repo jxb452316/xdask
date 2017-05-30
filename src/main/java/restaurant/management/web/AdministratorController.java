475814024ad15a03bee64d92a0d481e4d9e5e972
jxb <1195777571@qq.com>
2017-05-30 23:04:54 +0800

    fix

--- src/main/java/restaurant/management/web/AdministratorController.java
+++ src/main/java/restaurant/management/web/AdministratorController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdministratorController {
-
+    @RequestMapping(value = "/adminEE", method = RequestMethod.GET)
+    public String adminMM() {
+        return "adminEE";
+    }
 
 }

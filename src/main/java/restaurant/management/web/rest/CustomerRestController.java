4774be972a058304ca7469ab06c0623f88f4e7a7
jxb <1195777571@qq.com>
2017-06-02 14:06:39 +0800

    fix

--- src/main/java/restaurant/management/web/rest/CustomerRestController.java
+++ src/main/java/restaurant/management/web/rest/CustomerRestController.java
@@ -2,7 +2,9 @@ package restaurant.management.web.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import restaurant.management.model.Customer;
 import restaurant.management.model.Employee;
+import restaurant.management.service.CustomerService;
 import restaurant.management.service.EmployeeService;
 import restaurant.management.utils.CommonUtils;
 
@@ -16,44 +18,41 @@ import java.util.List;
  * 2017/6/1 - created by zhe.jiang
  */
 @RestController
-@RequestMapping("api/employee")
+@RequestMapping("api/customer")
 public class CustomerRestController {
 
     @Autowired
-    private EmployeeService employeeService;
+    private CustomerService customerService;
 
     @RequestMapping(value = "search", method = { RequestMethod.POST, RequestMethod.GET })
-    public List<Employee> search(@RequestParam(name = "emname", required = false, defaultValue = "") String emname,
-                                 @RequestParam(name = "emidcard", required = false, defaultValue = "") String emidcard,
-                                 @RequestParam(name = "emcellphone", required = false, defaultValue = "") String emcellphone,
-                                 @RequestParam(name = "emage", required = false, defaultValue = "") String emage,
-                                 @RequestParam(name = "emsex", required = false, defaultValue = "") String emsex,
-                                 @RequestParam(name = "emaddress", required = false, defaultValue = "") String emaddress) {
-        return employeeService.filter(
-                CommonUtils.nullIfEmpty(emname),
-                CommonUtils.nullIfEmpty(emidcard),
-                CommonUtils.nullIfEmpty(emcellphone),
-                CommonUtils.nullIfEmpty(emage) == null ? null : Integer.valueOf(emage),
-                CommonUtils.nullIfEmpty(emsex) == null ? null : Boolean.valueOf(emsex),
-                CommonUtils.nullIfEmpty(emaddress)
+    public List<Customer> search(@RequestParam(name = "cusname", required = false, defaultValue = "") String cusname,
+                                 @RequestParam(name = "cusidcard", required = false, defaultValue = "") String cusidcard,
+                                 @RequestParam(name = "cuscellphone", required = false, defaultValue = "") String cuscellphone,
+                                 @RequestParam(name = "money", required = false, defaultValue = "") String money)
+                                 {
+        return customerService.filter(
+                CommonUtils.nullIfEmpty(cusname),
+                CommonUtils.nullIfEmpty(cusidcard),
+                CommonUtils.nullIfEmpty(cuscellphone),
+                CommonUtils.nullIfEmpty(money) == null ? null : Double.valueOf(money)
         );
     }
 
     @RequestMapping(value = "save", method = { RequestMethod.POST })
-    public Employee save(@RequestBody Employee employee) {
-        employeeService.saveEmployee(employee);
-        return employee;
+    public Customer save(@RequestBody Customer customer) {
+        customerService.saveCustomer(customer);
+        return customer;
     }
 
     @RequestMapping(value = "update", method = { RequestMethod.POST })
-    public Employee update(@RequestBody Employee employee) {
-        employeeService.updateEmployee(employee);
-        return employee;
+    public Customer update(@RequestBody Customer customer) {
+        customerService.updateCustomer(customer);
+        return customer;
     }
 
     @RequestMapping(value = "delete/{id}", method = { RequestMethod.DELETE })
     public OperationResponse delete(@PathVariable(name = "id") Long id) {
-        employeeService.deleteEmployeeById(id);
+        customerService.deleteCustomerById(id);
         OperationResponse response = new OperationResponse();
         response.setSuccess(true);
         return response;
